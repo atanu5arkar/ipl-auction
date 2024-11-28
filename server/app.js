ad1ba3c29bd322618f7950d3dd41c3cbb94865b4
atanu5arkar <atanu_sarkar1@outlook.com>
2024-11-28 06:41:17 +0000

    adds cors and home route

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 import "./utils/dbConnect.js";
 import TeamModel from "./models/Team.js";
+import PlayerModel from "./models/Player.js";
 
 const app = express();
 const port = 8000;
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(cors());
+
 app.get('/', async (req, res) => {
     try {
-        await TeamModel.find({})
+        const teamData = await TeamModel.find({}).populate('players');
+        return res.status(200).json(teamData);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ msg: 'Internal Server Error!' });
