84e16e57c906cf1d643255fda5107dc7eb4f4383
atanu5arkar <atanu_sarkar1@outlook.com>
2024-11-28 16:36:43 +0000

    mods the db connection string

--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://atanusarkar1:Bootcamp@cs23.2jvvw4x.mongodb.net/ipl3');
+        await mongoose.connect('mongodb+srv://atanusarkar1:Bootcamp@cs23.2jvvw4x.mongodb.net/ipl2');
         console.log('Connected to Mongo.');
     } catch (error) {
         console.log('Unable to connect Mongo.');
