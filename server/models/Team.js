2da9e252894b560575791a287a43ced8f5d5d86d
atanu5arkar <atanu_sarkar1@outlook.com>
2024-11-28 04:37:12 +0000

    ipl auction: initial commit

---
+++ server/models/Team.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const teamSchema = new mongoose.Schema({
+    team: {
+        type: String,
+        required: true
+    },
+    teamid: {
+        type: Number,
+        required: true
+    },
+    players: [{ 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'player'
+    }]
+});
+
+const TeamModel = mongoose.model('team', teamSchema, 'teams');
+export default TeamModel;
