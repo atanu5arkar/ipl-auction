eec940b5506494a9e0de49956d40829b6badfe81
atanu5arkar <atanu_sarkar1@outlook.com>
2024-11-30 07:35:11 +0530

    adds loading and alerts to the UI

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import spinner from "../assets/spinner.gif";
+
+function Loading() {
+    return (
+        <center>
+            <img src={spinner} alt="loading animation" />
+        </center>
+    )
+}
+
+export default Loading;
